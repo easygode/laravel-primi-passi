c922cbf5add6d8afad918afb42aaacf0c24d6f32
easygode <bimalbenvenuti@gmail.com>
2023-05-02 10:03:08 +0200

    esercizio corretto

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -4,10 +4,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Laravel primi passi</title>
+    <title>Laravel Primi Passi</title>
 </head>
 <body>
-    <h1>Welcome Classe 89</h1>
-    <h2>Hello World</h2>
+    <h1>Hello World</h1>
 </body>
 </html>
